fe2cd0dacad6b0a92932a659ea713245af487b51
Alexis Dub <86374224+Shybaaaa@users.noreply.github.com>
2024-03-29 13:31:02 +0100

    update

--- public/views/dashboard/myaccount.php
+++ public/views/dashboard/myaccount.php
@@ -1,3 +1,16 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" and isset($_POST["update"])) {
+    $username = htmlspecialchars(trim($_POST["username"]));
+    $email = htmlspecialchars(trim($_POST["email"]));
+    $password = htmlspecialchars(trim($_POST["password"]));
+    $new_password = htmlspecialchars(trim($_POST["new_password"]));
+    $new_password_confirm = htmlspecialchars(trim($_POST["new_password_confirm"]));
+    $bio = htmlspecialchars(trim($_POST["bio"]));
+}
+
+?>
+
 <div class="w-11/12 h-5/6 rounded-lg bg-white px-2 py-2.5">
     <div class="flex flex-col h-full">
         <h2 class="mt-2 ml-1.5 text-2xl text-gray-700 font-bold border-b-2 border-opacity-50 bg-clip-border border-gray-10">Informations Personnelles </h2>
