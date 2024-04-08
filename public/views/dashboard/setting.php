5082f9ca0f9808e34f231e9f788d92ff3d2a5a25
A-Meuret <148949079+A-Meuret@users.noreply.github.com>
2024-04-08 09:35:26 +0200

    fin delete

--- public/views/dashboard/setting.php
+++ public/views/dashboard/setting.php
@@ -25,31 +25,30 @@ $users = getUser($_SESSION["user"]["id"]);
 
 
     <div class="w-full mb-4 select-none">
-            <?php if (isset($_SESSION["user"]) && $_SESSION["user"]): ?>
-                <div class="flex items-center gap-3 rounded p-1 w-full ">
-                    <?php if ($_SESSION["user"]["image"]): ?>
-                        <img class="w-10 h-10 rounded-full" src="<?= $_SESSION["user"]["image"] ?>" alt="">
-                    <?php else: ?>
-                        <i class="fa-solid fa-user text-xl rounded-full p-3 bg-gray-500 text-white"></i>
-                    <?php endif; ?>
-                    <div class="font-medium dark:text-white">
-                        <div><?= $users["username"] ?></div>
-                        <div class="text-sm text-gray-500 dark:text-gray-400"><?= $role["name"] ?></div>
-                    </div>
-                        <a class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-yellow-300 dark:hover:bg-yellow-400 dark:focus:ring-yellow-500" href="../../../public/views/dashboard/delete.php">modifier le compte</a>
-                        <?php
-                        if ($users["isActive"] == 1) {
-                        ?>
-                        <a class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800" href="../../../public/views/dashboard/delete.php?id=<?= $users["id"] ?>">supprimer le compte</a>
-                        <?php
-                        } else {
-                        ?>
-                        <a class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" href="../../../public/views/dashboard/delete.php?id=<?= $users["id"] ?>">restaurer le compte</a>
-                        <?php
-                        }
-                        ?>
-                </div>
+        <?php if (isset($_SESSION["user"]) && $_SESSION["user"]): ?>
+            <div class="flex items-center gap-3 rounded p-1 w-full ">
+                <?php if ($_SESSION["user"]["image"]): ?>
+                    <img class="w-10 h-10 rounded-full" src="<?= $_SESSION["user"]["image"] ?>" alt="">
+                <?php else: ?>
+                    <i class="fa-solid fa-user text-xl rounded-full p-3 bg-gray-500 text-white"></i>
                 <?php endif; ?>
+                <div class="font-medium dark:text-white">
+                    <div><?= $users["username"] ?></div>
+                    <div class="text-sm text-gray-500 dark:text-gray-400"><?= $role["name"] ?></div>
+                </div>
+                    <?php
+                    if ($users["isActive"] == 1) {
+                    ?>
+                    <a class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800" href="../../../public/views/dashboard/delete.php?id=<?= $users["id"] ?>">supprimer le compte</a>
+                    <?php
+                    } else {
+                    ?>
+                    <a class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" href="../../../public/views/dashboard/delete.php?id=<?= $users["id"] ?>">restaurer le compte</a>
+                    <?php
+                    }
+                    ?>
+            </div>
+            <?php endif; ?>
     </div>
 
     <?php require_once "../../../public/include/footer.php" ?>
