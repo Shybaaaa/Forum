c70afccdc27bbb8ca694a92e1cdf7636aa8268e5
Alexis Dub <86374224+Shybaaaa@users.noreply.github.com>
2024-04-12 11:21:05 +0200

    Update login.php

--- public/views/login.php
+++ public/views/login.php
@@ -139,10 +139,10 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
 <?php if (isset($status) && $status["isActive"] == 0): ?>
 
-<div id="popup-modal" tabindex="-1" class="flex justify-center items-center overflow-y-auto fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+<div id="restoreModal" tabindex="-1" class="flex overflow-y-auto fixed top-0 right-0 left-0 backdrop-brightness-50 z-50 w-full h md:inset-0  max-h-full">
     <div class="relative p-4 w-full max-w-md max-h-full">
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <button type="button" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal">
+            <button type="button" data-modal-target="restoreModal"  class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="restoreModal">
                 <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                 </svg>
@@ -153,14 +153,14 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
                 </svg>
                 <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Votres comptes a était désactiver, voulez vous le reactiver</h3>
-                <button data-modal-hide="popup-modal" type="button" name="restore" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
-                    Oui
-                </button>
-                <button data-modal-hide="popup-modal" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-                    Non, me connecter ou crée un autre compte</button>
-                    </form>
+                <div class="flex flex-row items-center justify-center gap-x-1">
+                    <button data-modal-hide="restoreModal" data-modal-target="restoreModal" type="button" name="restore" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                        Oui
+                    </button>
+                    <button data-modal-hide="restoreModal" data-modal-target="restoreModal" type="button" class="py-2.5 px-5 ms-3 text-xs font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                        Me connecter ou créer un autre compte
+                    </button>
                 </div>
-
             </div>
         </div>
     </div>
