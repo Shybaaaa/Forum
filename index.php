2a9e3ffa31a446c8050ee3604e0f58d856a1667d
Alexis Dub <86374224+Shybaaaa@users.noreply.github.com>
2024-04-17 13:27:52 +0200

    refonte de l'interface v0.0.1

--- index.php
+++ index.php
@@ -8,6 +8,10 @@ if (isset($_GET["disconnect"]) && $_GET["disconnect"] == 1) {
     disconnect();
 }
 
+if (isset($_SESSION["user"])) {
+    $role = getRole($_SESSION["user"]["id"]);
+}
+
 ?>
 
 <!doctype html>
@@ -21,9 +25,6 @@ if (isset($_GET["disconnect"]) && $_GET["disconnect"] == 1) {
 </head>
 <body>
 
-<?php require_once("public/include/dark_mode.php") ?>
-<?php require_once("public/include/navbar.php") ?>
-<?php require_once("public/views/home.php") ?>
 <?php if (isset($_GET["success"]) and $_GET["success"] == 1): ?>
     <div id="toast-success"
          class="fixed top-20 right-10 flex items-center w-full max-w-sm p-4 mb-4 text-gray-500 bg-white rounded-lg shadow"
@@ -45,24 +46,92 @@ if (isset($_GET["disconnect"]) && $_GET["disconnect"] == 1) {
         </button>
     </div>
 <?php endif; ?>
-<div class="app">
-    <?php
-        if (isset($_GET["page"])) {
-            switch ($_GET["page"]) {
-                case "home":
-                    require_once "public/views/home.php";
-                    break;
-                default:
-                    require_once "public/views/home.php";
-                    break;
-            }
-        } else {
-            require_once "public/views/home.php";
-        }
-    ?>
+
+<div>
+    <div class="hidden xl:fixed xl:inset-y-0 xl:z-50 xl:flex xl:w-72 xl:flex-col">
+        <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-100 px-6 rounded-r-2xl ring-1 ring-white/5">
+            <div class="flex h-16 shrink-0 items-center">
+                <h4 class="text-lg font-semibold text-gray-900 dark:text-white"><?= $config["APP_NAME"] ?></h4>
+            </div>
+            <nav class="flex flex-1 flex-col">
+                <ul role="list" class="flex flex-1 flex-col gap-y-7">
+                    <li>
+                        <ul role="list" class="-mx-2 space-y-1">
+                            <li>
+                                <a href="index.php?page=home" class="text-gray-700 hover:text-gray-600 group flex items-center gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold">
+                                    <i class="fa-solid fa-house"></i>
+                                    Accueil
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="-mx-6 mt-auto">
+                        <?php if (isset($_SESSION["user"]) && $_SESSION["user"]): ?>
+                            <div type="button" data-dropdown-toggle="userDropdown" data-dropdown-placement="bottom-start" class="flex items-center gap-3 rounded px-6 mx-auto bg-red-600 py-2 w-full mb-5">
+                                <?php if ($_SESSION["user"]["image"]): ?>
+                                    <img class="w-10 h-10 rounded-full" src="<?= $_SESSION["user"]["image"] ?>" alt="">
+                                <?php else: ?>
+                                    <i class="fa-solid fa-user text-xl rounded-full p-3 bg-gray-500 text-white"></i>
+                                <?php endif; ?>
+                                <div class="font-medium">
+                                    <div><?= $_SESSION["user"]["username"] ?></div>
+                                    <div class="text-sm text-gray-500"><?= $role["name"] ?></div>
+                                </div>
+                            </div>
+                            <div id="userDropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                                <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="avatarButton">
+                                    <li>
+                                        <a href="/public/views/dashboard/index.php?page=myaccount" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Tableau de bord</a>
+                                    </li>
+                                    <li>
+                                        <a href="/public/views/dashboard/setting.php?page=setting" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Paramètres</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Support</a>
+                                    </li>
+                                </ul>
+                                <div class="py-1">
+                                    <a href="/index.php?disconnect=1" class="text-red-600 block px-4 py-2 text-sm hover:text-red-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                                        <i class="fa-solid fa-right-from-bracket font-light ml-1.5"></i>
+                                        Déconnexion
+                                    </a>
+                                </div>
+                            </div>
+                        <?php else: ?>
+                            <a href="/public/views/login.php" class="flex font-medium p-2 items-center text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                <i class="fa-solid fa-right-to-bracket text-gray-500 group-hover:text-gray-900 duration-75 transition"></i>
+                                <span class="flex-1 ms-3">Connexion</span>
+                            </a>
+                        <?php endif; ?>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+
+    <div class="xl:pl-72">
+        <main>
+
+        </main>
+    </div>
 </div>
 
 
 <?php require_once("public/include/footer.php") ?>
 </body>
 </html>
+
+<?php
+//if (isset($_GET["page"])) {
+//    switch ($_GET["page"]) {
+//        case "home":
+//            require_once "public/views/home.php";
+//            break;
+//        default:
+//            require_once "public/views/home.php";
+//            break;
+//    }
+//} else {
+//    require_once "public/views/home.php";
+//}
+?>
