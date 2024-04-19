bb04aedeccdb178fd1e7c450dab12d04293f0c8d
Alexis Dub <86374224+Shybaaaa@users.noreply.github.com>
2024-04-19 11:02:49 +0200

    régalge bug surposition + avancement UI/UX home

--- public/views/home.php
+++ public/views/home.php
@@ -1,6 +1,7 @@
 <?php
 
 $categorys = getCategory(-1);
+$post
 
 ?>
 
@@ -8,13 +9,11 @@ $categorys = getCategory(-1);
     <?php foreach($categorys as $category): ?>
         <section class="w-[80%] h-fit rounded-lg shadow-md  max-h-fit bg-white mx-auto gap-x-7 gap-y-9 px-6 py-4 overflow-x-hidden">
             <div class="mb-2 flex flex-row items-center justify-between">
-                <h1 class="text-lg text-black font-bold"><a href="/index.php?page=viewCategory&ref=<?=$category["reference"]?>"><?= ucfirst($category["name"]) ?></a></h1>
+                <h1 class="text-lg text-black font-bold"><a href="/index.php?page=viewCategory&ref=<?=$category["reference"]?>" class="inline-flex items-center"><i class="fa-solid fa-<?= $category["icons"]?> bg-indigo-500 text-white p-2 rounded-full shadow-md  mr-3"></i><?= ucfirst($category["name"]) ?></a></h1>
                 <a href="/index.php?page=viewCategory&ref=<?=$category["reference"]?>" class="text-sm font-sans font-medium text-indigo-600 hover:text-indigo-500 transition-all">Voir plus</a>
             </div>
             <div>
-                <article>Art1</article>
-                <article>Art2</article>
-                <article>Art3</article>
+                <?php print_r(getPostsWhereCat($category["id"], 3, "desc")); ?>
             </div>
         </section>
     <?php endforeach; ?>
