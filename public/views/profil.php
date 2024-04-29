31c095b4e9d79edf3fa9431d1c50038500e05629
Alexis Dub <86374224+Shybaaaa@users.noreply.github.com>
2024-04-29 09:39:02 +0200

    update profil résolution de bug

--- public/views/profil.php
+++ public/views/profil.php
@@ -17,14 +17,16 @@ $user = getUserByRef($_GET["ref"]);
         <div class="w-full h-[20%] bg-gray-200 rounded-md"></div>
         <div class="-translate-y-1/2">
             <div class="flex flex-row items-center justify-center gap-x-4">
-                <div class="w-[15%] h-[15%] flex flex-col space-y-3 items-center">
-                    <?php if(isset($user["image"]) && $user["image"] != ""): ?>
-                        <img src="<?= $user["image"] ?>" alt="avatar" class="relative shadow-md border-2 inline-block object-cover object-center h-[160px] w-[160px] min-w-full min-h-full rounded-full" />
-                    <?php else: ?>
-                        <span class="bg-gray-400 flex items-center text-8xl shadow-md rounded-full text-white min-w-full min-h-full justify-center">
-                            <i class="fa-solid p-8 fa-user"></i>
-                        </span>
-                    <?php endif; ?>
+                <div class="w-[30%] h-[15%] flex flex-col space-y-3 items-center">
+                    <div class="w-full px-6 py-2">
+                        <?php if(isset($user["image"]) && $user["image"] != ""): ?>
+                            <img src="<?= $user["image"] ?>" alt="avatar" class="w-32 h-32 object-cover rounded-full shadow-md border-2" />
+                        <?php else: ?>
+                            <span class="w-32 h-32 bg-gray-400 flex items-center justify-center text-8xl shadow-md rounded-full text-white">
+                                <i class="fa-solid p-8 fa-user"></i>
+                            </span>
+                        <?php endif; ?>
+                    </div>
                     <div class="inline-flex text-center flex-col items-center ">
                         <span class="text-2xl font-semibold"><?= $user["username"] ?></span>
                         <?php
